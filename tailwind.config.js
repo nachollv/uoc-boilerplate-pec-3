bf34236ff8672c82093a874a20aef836e48b4f7e
ignacio lladó vidal <79189683+nachollv@users.noreply.github.com>
2022-12-26 10:38:11 +0100

    he instalado fontawesome-free, customizado fuentes de texto

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
       colors: {
         'uoc-corporate': '#b91b0b',
         'uoc-masterbrand': '#f6e9d6',
-      }
+      },
+      fontFamily: {
+        sans: [ "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto",
+        "Helvetica Neue", "Arial", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji",
+        "Segoe UI Symbol"],
+        serif: ["Georgia", "Times New Roman", "serif"],
+      },
     },
     container: {
       center: true,
